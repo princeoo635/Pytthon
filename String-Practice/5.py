687acc527e212853192515e7445fad4d7a922ee3
princeoo635 <princeoo635@gmail.com>
2026-01-10 22:14:06 +0530

    Add character frequency counting implementation

--- String-Practice/5.py
+++ String-Practice/5.py
@@ -1,1 +1,9 @@
 # 5. Count the frequency of each character in a string.
+word=input("enter your word: ")
+freq= {}
+for c in word:
+    if c in freq:
+        freq[c]=freq[c] +1
+    else:    
+        freq[c]=1
+print(freq)
